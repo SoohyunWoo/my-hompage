137512f5284cf04c0052872500af93194974429e
SoohyunWoo <sa0110984@gmail.com>
2025-08-20 17:52:13 +0900

    Update vite.config.js

--- soohyun-portfolio/vite.config.js
+++ soohyun-portfolio/vite.config.js
@@ -1,8 +1,7 @@
-// If your repo name is different, change "/Portpolio/" accordingly.
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Portpolio/",
   plugins: [react()],
+  base: '/my-homepage/',   // <- 레포 이름과 동일
 })
